64632f3d2805e58c4408c69c92dd12ba1caca632
Abdallahhany <abdallahhany253@gmail.com>
2022-08-29 22:07:57 +0200

    design database

---
+++ .env
@@ -0,0 +1,6 @@
+POSTGRES_HOST=127.0.0.1
+POSTGRES_DB=shopping
+POSTGRES_TEST_DB=shopping_test
+POSTGRES_USER=shopping_user
+POSTGRES_PASSWORD=password123
+ENV=dev
---
+++ migrations/sqls/20220829194442-user-table-up.sql
@@ -0,0 +1,7 @@
+/* Replace with your SQL commands */
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  firstName VARCHAR(50) NOT NULL,
+  lastName VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL
+);
